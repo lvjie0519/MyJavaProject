69aa51d7f0502f8fcf0cd255297cafc2f1f473a4
lvjie <991102@nd.com>
2017-12-14 16:11:51 +0800

    synch  关键字的作用

---
+++ src/thread/demo/SyncTest.java
@@ -0,0 +1,59 @@
+package thread.demo;
+
+/**
+ * Created by Administrator on 2017/12/14 0014.
+ */
+public class SyncTest {
+
+    public static void main(String []args){
+
+        MySync mySync = new MySync();
+
+        MyThread t1 = new MyThread(mySync);
+        MyThread t2 = new MyThread(mySync);
+
+        t1.start();
+        t2.start();
+    }
+
+
+}
+
+class MySync{
+
+    private int count = 0;
+
+    public void execute(){
+
+        synchronized (this){
+            for(int i=0; i<5; i++){
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName()+":  count="+count++);
+            }
+        }
+
+    }
+
+
+}
+
+class MyThread extends Thread
+{
+    private MySync mySync;
+
+    public MyThread(MySync sync)
+    {
+        this.mySync = sync;
+    }
+
+    @Override
+    public void run()
+    {
+        mySync.execute();
+    }
+
+}
