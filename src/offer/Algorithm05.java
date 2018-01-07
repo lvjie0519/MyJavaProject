c0cec1167f9cab41bc125d5536b8c57b4e3c4d7a
lvjie <991102@nd.com>
2018-01-07 17:19:32 +0800

    斐波那契数列

---
+++ src/offer/Algorithm05.java
@@ -0,0 +1,46 @@
+package offer;
+
+
+/**
+ * Created by Administrator on 2017/10/15 0015.
+ * 大家都知道斐波那契数列，现在要求输入一个整数n，请你输出斐波那契数列的第n项。
+ */
+public class Algorithm05 {
+
+    public static void main(String []args){
+
+        System.out.println(Fibonacci1(39));
+
+    }
+
+    public static int Fibonacci(int n) {
+
+        if(n == 1){
+            return 0;
+        }else if(n == 2){
+            return 1;
+        }else return Fibonacci(n-1)+ Fibonacci(n-2);
+
+    }
+
+    public static int Fibonacci1(int n) {
+
+        if(n<0){
+            return 0;
+        }
+
+        if(n == 0 || n==1){
+            return n;
+        }
+
+        int num1 = 0, num2 = 1;
+        for(int i=2; i<=n; i++){
+            num2 = num1+num2;
+            num1 = num2-num1;
+        }
+
+
+        return num2;
+    }
+
+}
