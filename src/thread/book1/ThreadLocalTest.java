5ba553af18831ec7976505ddc2fb34159422b174
lvjie1 <lvjie1@xiaomi.com>
2020-06-13 22:40:43 +0800

    add threadlocal

---
+++ src/thread/book1/ThreadLocalTest.java
@@ -0,0 +1,14 @@
+package thread.book1;
+
+/**
+ * ThreadLocal
+ */
+public class ThreadLocalTest {
+
+    public static void main(String []args){
+    }
+
+    private static void test(){
+        ThreadLocal threadLocal = new ThreadLocal();
+    }
+}
